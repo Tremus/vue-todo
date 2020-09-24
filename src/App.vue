05e4cbf44b66831d6ac4c695dceac28554d10036
Tremus <tredudman@yahoo.com.au>
2020-09-24 12:33:46 +0100

    Added views folder

--- src/App.vue
+++ src/App.vue
@@ -1,90 +1,24 @@
-<template>
-    <div id="app">
-        <Modal v-if="modal" v-bind:close="toggleModal">
-            <Form slot="content" v-bind:postTask="postTask"></Form>
-        </Modal>
-        <img src="./assets/logo.png" />
-        <h1>{{ msg }}</h1>
-        <h2>Tasks</h2>
-        <div class="task-list-container">
-            <ul>
-                <Task
-                    v-for="task in tasks"
-                    v-bind:key="task.id"
-                    v-bind:id="task.id"
-                    v-bind:name="task.name"
-                    v-bind:description="task.description"
-                    v-bind:completed="task.completed"
-                    v-bind:createdAt="task.createdAt"
-                    v-bind:updatedAt="task.updatedAt"
-                    v-bind:patchTask="patchTask(task)"
-                ></Task>
-            </ul>
-            <button @click="toggleModal()">New Task</button>
-        </div>
-    </div>
-</template>
-
 <script>
-import Task from './Task';
-import Modal from './Modal';
-import Form from './Form';
+import Home from './views/Home/Home';
+
+const routes = {
+    '/': Home,
+};
 
 export default {
     name: 'app',
-    components: { Task, Modal, Form },
     data() {
         return {
-            msg: 'Welcome to Your Vue.js App',
-            tasks: [],
-            modal: false,
+            currentRoute: window.location.pathname,
         };
     },
-    mounted() {
-        this.getTasks();
+    computed: {
+        ViewComponent() {
+            return routes[this.currentRoute];
+        },
     },
-    methods: {
-        getTasks() {
-            fetch('http://localhost:8080/api/tasks')
-                .then(res => res.json())
-                .then(data => {
-                    this.tasks = data.tasks;
-                });
-        },
-        patchTask(task) {
-            return () => {
-                const body = { completed: !task.completed };
-                fetch(`http://localhost:8080/api/tasks/${task.id}`, {
-                    method: 'PATCH',
-                    headers: {
-                        ['Content-Type']: 'application/json',
-                    },
-                    body: JSON.stringify(body),
-                })
-                    .then(res => res.json())
-                    .then(data => {
-                        task.completed = data.task.completed;
-                    });
-            };
-        },
-        postTask(name, description) {
-            const body = { name, description };
-            fetch('http://localhost:8080/api/tasks', {
-                method: 'POST',
-                headers: {
-                    ['Content-Type']: 'application/json',
-                },
-                body: JSON.stringify(body),
-            })
-                .then(res => res.json())
-                .then(data => {
-                    this.tasks.push(data.task);
-                    this.modal = false;
-                });
-        },
-        toggleModal() {
-            this.modal = !this.modal;
-        },
+    render(h) {
+        return h(this.ViewComponent);
     },
 };
 </script>
@@ -97,10 +31,6 @@ export default {
     text-align: center;
     color: #2c3e50;
     margin-top: 60px;
-}
-.task-list-container {
-    max-width: 400px;
-    margin: auto;
 }
 li {
     list-style: none;
